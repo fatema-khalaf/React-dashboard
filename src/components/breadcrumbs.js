187ad36de0c02ca037d293471caf349c5686b92b
fatema-khalaf <www.fatemakhalaf2015@gmail.com>
2022-07-28 21:03:36 +0300

    Create Custom breadcumbers component

---
+++ src/components/breadcrumbs.js
@@ -0,0 +1,52 @@
+// material
+import { styled } from '@mui/material/styles';
+import { Box, Link, Stack, Breadcrumbs, Typography, ListItemIcon } from '@mui/material';
+
+export default function CusBreadcrumbs({ title, links, page }) {
+  const ListItemIconStyle = styled(ListItemIcon)({
+    minWidth: 12,
+    height: 12,
+    color: 'inherit',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  });
+  const separator = (
+    <ListItemIconStyle>
+      <Box
+        component="span"
+        sx={{
+          width: 4,
+          height: 4,
+          display: 'flex',
+          borderRadius: '50%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          bgcolor: 'text.disabled',
+        }}
+      />
+    </ListItemIconStyle>
+  );
+  return (
+    <Box direction="row" alignItems="center" justifyContent="space-between" mb={4}>
+      <Box>
+        <Typography variant="h4" gutterBottom>
+          {title}
+        </Typography>
+        <Breadcrumbs disableTypography separator={separator} aria-label="breadcrumb">
+          {links.map((link) => (
+            <Typography variant="body2" color="text.primary">
+              <Link underline="hover" color="inherit" href={link.path}>
+                {link.name}
+              </Link>
+            </Typography>
+          ))}
+
+          <Typography variant="body2" color="text.disabled">
+            {page}
+          </Typography>
+        </Breadcrumbs>
+      </Box>
+    </Box>
+  );
+}
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,6 +1,129 @@
-import Page from '../components/Page';
+import { Link as RouterLink } from 'react-router-dom';
+// material
+import { alpha, useTheme, styled } from '@mui/material/styles';
+
+import {
+  Link,
+  Card,
+  Table,
+  Stack,
+  Avatar,
+  Button,
+  Checkbox,
+  TableRow,
+  TableBody,
+  TableCell,
+  Container,
+  Typography,
+  TableContainer,
+  TablePagination,
+  Box,
+  ListItemIcon,
+} from '@mui/material';
 import { CreateCard } from '../sections/@dashboard/create';
 
+//
+import Page from '../components/Page';
+import CusBreadcrumbs from '../components/breadcrumbs';
+
 export default function Create() {
-  return <CreateCard />;
+  return (
+    <Page title="Brand | Create">
+      <Container>
+        <CusBreadcrumbs
+          title="Create a new brand"
+          links={[
+            { name: 'Dashboard', path: '/' },
+            { name: 'Brands', path: '/' },
+          ]}
+          page="New brand"
+        />
+        {/* <Card>
+          <UserListToolbar numSelected={selected.length} filterName={filterName} onFilterName={handleFilterByName} />
+
+          <Scrollbar>
+            <TableContainer sx={{ minWidth: 800 }}>
+              <Table>
+                <UserListHead
+                  order={order}
+                  orderBy={orderBy}
+                  headLabel={TABLE_HEAD}
+                  rowCount={USERLIST.length}
+                  numSelected={selected.length}
+                  onRequestSort={handleRequestSort}
+                  onSelectAllClick={handleSelectAllClick}
+                />
+                <TableBody>
+                  {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                    const { id, name, role, status, company, avatarUrl, isVerified } = row;
+                    const isItemSelected = selected.indexOf(name) !== -1;
+
+                    return (
+                      <TableRow
+                        hover
+                        key={id}
+                        tabIndex={-1}
+                        role="checkbox"
+                        selected={isItemSelected}
+                        aria-checked={isItemSelected}
+                      >
+                        <TableCell padding="checkbox">
+                          <Checkbox checked={isItemSelected} onChange={(event) => handleClick(event, name)} />
+                        </TableCell>
+                        <TableCell component="th" scope="row" padding="none">
+                          <Stack direction="row" alignItems="center" spacing={2}>
+                            <Avatar alt={name} src={avatarUrl} />
+                            <Typography variant="subtitle2" noWrap>
+                              {name}
+                            </Typography>
+                          </Stack>
+                        </TableCell>
+                        <TableCell align="left">{company}</TableCell>
+                        <TableCell align="left">{role}</TableCell>
+                        <TableCell align="left">{isVerified ? 'Yes' : 'No'}</TableCell>
+                        <TableCell align="left">
+                          <Label variant="ghost" color={(status === 'banned' && 'error') || 'success'}>
+                            {sentenceCase(status)}
+                          </Label>
+                        </TableCell>
+
+                        <TableCell align="right">
+                          <UserMoreMenu />
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                  {emptyRows > 0 && (
+                    <TableRow style={{ height: 53 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
+                </TableBody>
+
+                {isUserNotFound && (
+                  <TableBody>
+                    <TableRow>
+                      <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+                        <SearchNotFound searchQuery={filterName} />
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                )}
+              </Table>
+            </TableContainer>
+          </Scrollbar>
+
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={USERLIST.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Card> */}
+      </Container>
+    </Page>
+  );
 }

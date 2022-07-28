3f1053e8ca01eab4d97312e2a5a43463049031fc
fatema-khalaf <www.fatemakhalaf2015@gmail.com>
2022-07-28 18:54:41 +0300

    Addin initial files

---
+++ src/pages/Create.js
@@ -0,0 +1,6 @@
+import Page from '../components/Page';
+import { CreateCard } from '../sections/@dashboard/create';
+
+export default function Create() {
+  return <CreateCard />;
+}
---
+++ src/sections/@dashboard/create/CreateCard.js
@@ -0,0 +1,8 @@
+import PropTypes from 'prop-types';
+import { Link as RouterLink } from 'react-router-dom';
+import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
+import NavSection from '../../../components/NavSection';
+// ----------------------------------------------------------------------
+export default function CreateCard() {
+  return <div>Create card</div>;
+}

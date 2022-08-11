4381ccf2e2604845397a8d20df7f5c4d979c771b
fatema-khalaf <www.fatemakhalaf2015@gmail.com>
2022-08-11 18:01:27 +0300

    Protect all routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { Navigate, useRoutes } from 'react-router-dom';
+import Cookies from 'universal-cookie';
 // layouts
 import DashboardLayout from './layouts/dashboard';
 import LogoOnlyLayout from './layouts/LogoOnlyLayout';
@@ -14,48 +15,55 @@ import DashboardApp from './pages/DashboardApp';
 import CategoryList from './pages/category/CategoryList';
 import EditCategory from './pages/category/EditCategory';
 import CreateCategory from './pages/category/CreateCategory';
+import ProtectedRoute from './sections/ProtectedRouts';
 
 // ----------------------------------------------------------------------
 
 export default function Router() {
-  return useRoutes([
-    {
-      path: '/dashboard/brand',
-      element: <DashboardLayout />,
-      children: [
-        { path: 'create', element: <CreateBrand /> },
-        { path: 'list', element: <ListBrand /> },
-        { path: 'edit/:id', element: <EditBrand /> },
-      ],
-    },
-    {
-      path: '/dashboard/category',
-      element: <DashboardLayout />,
-      children: [
-        { path: 'create', element: <CreateCategory /> },
-        { path: 'list', element: <CategoryList /> },
-        { path: 'edit/:id', element: <EditCategory /> },
-      ],
-    },
-    {
-      path: '/dashboard',
-      element: <DashboardLayout />,
-      children: [
-        { path: 'app', element: <DashboardApp /> },
-        { path: 'user', element: <User /> },
-      ],
-    },
-    {
-      path: '/',
-      element: <LogoOnlyLayout />,
-      children: [
-        { path: '/', element: <Navigate to="/dashboard/app" /> },
-        { path: 'login', element: <Login /> },
-        { path: 'register', element: <Register /> },
-        { path: '404', element: <NotFound /> },
-        { path: '*', element: <Navigate to="/404" /> },
-      ],
-    },
-    { path: '*', element: <Navigate to="/404" replace /> },
-  ]);
+  const cookies = new Cookies();
+  const isAuthenticated = cookies.get('token');
+  return useRoutes(
+    isAuthenticated
+      ? [
+          {
+            path: '/dashboard/brand',
+            element: <DashboardLayout />,
+            children: [
+              { path: 'create', element: <CreateBrand /> },
+              { path: 'list', element: <ListBrand /> },
+              { path: 'edit/:id', element: <EditBrand /> },
+            ],
+          },
+          {
+            path: '/dashboard/category',
+            element: <DashboardLayout />,
+            children: [
+              { path: 'create', element: <CreateCategory /> },
+              { path: 'list', element: <CategoryList /> },
+              { path: 'edit/:id', element: <EditCategory /> },
+            ],
+          },
+          {
+            path: '/dashboard',
+            element: <DashboardLayout />,
+            children: [
+              { path: 'app', element: <DashboardApp /> },
+              { path: 'user', element: <User /> },
+            ],
+          },
+          {
+            path: '/',
+            element: <LogoOnlyLayout />,
+            children: [
+              { path: '/', element: <ProtectedRoute /> },
+              { path: 'login', element: <Login /> },
+              { path: 'register', element: <Register /> },
+              { path: '404', element: <NotFound /> },
+              { path: '*', element: <Navigate to="/404" /> },
+            ],
+          },
+          { path: '*', element: <Navigate to="/404" replace /> },
+        ]
+      : [{ path: '*', element: <Login /> }]
+  );
 }

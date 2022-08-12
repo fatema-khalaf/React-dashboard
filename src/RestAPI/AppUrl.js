da6160c9210636d64958971ce1ba30e8755ac825
fatema-khalaf <www.fatemakhalaf2015@gmail.com>
2022-08-12 18:43:35 +0300

    Update AppURLs

--- src/RestAPI/AppUrl.js
+++ src/RestAPI/AppUrl.js
@@ -1,24 +1,9 @@
-import Cookies from 'universal-cookie';
-
 class AppUrl {
-  static BaseURL = 'http://127.0.0.1:8000/api/v1';
+  static Brands = '/brands';
 
-  static cookies = new Cookies();
+  static Categries = '/categories';
 
-  static config = {
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${this.cookies.get('token')}`,
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  };
-
-  static AllBrands = `${this.BaseURL}/brands`;
-
-  static Categries = `${this.BaseURL}/categories`;
-
-  static Subcategories = `${this.BaseURL}/subcategories`;
+  static Subcategories = '/subcategories';
 }
 
 export default AppUrl;

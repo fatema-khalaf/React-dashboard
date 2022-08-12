07344926d357de7f0a49644b14570c60cc9e731b
fatema-khalaf <www.fatemakhalaf2015@gmail.com>
2022-08-12 15:42:58 +0300

    Create select field component

---
+++ src/components/hook-form/RHFSelectField.js
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+// form
+import { useFormContext, Controller } from 'react-hook-form';
+// @mui
+import { TextField, MenuItem } from '@mui/material';
+import { useState } from 'react';
+
+// ----------------------------------------------------------------------
+
+RHFSelectField.propTypes = {
+  name: PropTypes.string,
+  options: PropTypes.array,
+};
+
+export default function RHFSelectField({ name, options, ...other }) {
+  const { control } = useFormContext();
+  const [option, setOption] = useState('');
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          fullWidth
+          select
+          value={option}
+          onChange={(e) => {
+            field.onChange(e.target.value);
+            setOption(e.target.value);
+          }}
+          error={!!error}
+          helperText={error?.message}
+          {...other}
+        >
+          {options.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      )}
+    />
+  );
+}

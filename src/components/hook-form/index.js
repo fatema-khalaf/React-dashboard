d51a1cf1fc7c69f69f9106e25b2005978247d067
fatema-khalaf <www.fatemakhalaf2015@gmail.com>
2022-08-14 16:27:48 +0300

    Create InputTags component

--- src/components/hook-form/index.js
+++ src/components/hook-form/index.js
@@ -3,3 +3,4 @@ export * from './RHFCheckbox';
 export { default as RHFTextField } from './RHFTextField';
 export { default as FormProvider } from './FormProvider';
 export { default as RHFSelectField } from './RHFSelectField';
+export { default as TagsInput } from './TagsInput';

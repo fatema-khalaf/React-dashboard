328262b017f2d8f7446a67dc436913ab8ab65fe5
fatema-khalaf <www.fatemakhalaf2015@gmail.com>
2022-08-16 15:11:22 +0300

    Add error styling

--- src/components/hook-form/TextEdiotor.js
+++ src/components/hook-form/TextEdiotor.js
@@ -1,9 +1,10 @@
 import { alpha, useTheme } from '@mui/material/styles';
 import { Editor } from 'react-draft-wysiwyg';
+import { Typography } from '@mui/material';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import { useFormContext, Controller } from 'react-hook-form';
 import PropTypes from 'prop-types';
-import { EditorState, convertToRaw, ContentState } from 'draft-js';
+import { EditorState, ContentState } from 'draft-js';
 import { useEffect, useState } from 'react';
 import htmlToDraft from 'html-to-draftjs';
 
@@ -14,12 +15,6 @@ TextEdiotor.propTypes = {
 export function TextEdiotor({ name, initialValue, useFormRegister, ...other }) {
   // Style----------------------------------------------------------
   const theme = useTheme();
-  const warpperStyle = {
-    overflow: 'hidden',
-    position: 'relative',
-    borderRadius: '8px',
-    border: `1px solid ${alpha(theme.palette.grey[500], 0.32)}`,
-  };
   const toolbarStyle = {
     marginBottom: '0px',
     border: 'none',
@@ -48,24 +43,42 @@ export function TextEdiotor({ name, initialValue, useFormRegister, ...other }) {
         name={name}
         control={control}
         render={({ field, fieldState: { error } }) => (
-          <Editor
-            {...field}
-            editorState={editorState}
-            value={typeof field.value === 'number' && field.value === 0 ? '' : field.value}
-            toolbarClassName="toolbarClassName"
-            toolbarStyle={toolbarStyle}
-            wrapperClassName="wrapperClassName"
-            wrapperStyle={warpperStyle}
-            editorClassName="editorClassName"
-            editorStyle={{ backgroundColor: 'white', padding: ' 0px 15px', minHeight: '100px' }}
-            options={('fontSize', 'fontFamily')}
-            toolbar={{
-              options: ['inline', 'blockType', 'fontSize', 'list', 'textAlign', 'history'],
-              inline: { options: ['bold', 'italic', 'underline', 'strikethrough'] },
-              list: { options: ['unordered', 'ordered'] },
-            }}
-            onEditorStateChange={onEditorStateChange}
-          />
+          <>
+            <Editor
+              helperText={error?.message}
+              {...field}
+              editorState={editorState}
+              value={typeof field.value === 'number' && field.value === 0 ? '' : field.value}
+              toolbarClassName="toolbarClassName"
+              toolbarStyle={toolbarStyle}
+              wrapperClassName="wrapperClassName"
+              wrapperStyle={{
+                overflow: 'hidden',
+                position: 'relative',
+                borderRadius: '8px',
+                borderColor: error && `${theme.palette.error.main}`,
+                border: `1px solid ${theme.palette.grey[300]}`,
+              }}
+              editorClassName="editorClassName"
+              editorStyle={{ backgroundColor: 'white', padding: ' 0px 15px', minHeight: '100px' }}
+              options={('fontSize', 'fontFamily')}
+              toolbar={{
+                options: ['inline', 'blockType', 'fontSize', 'list', 'textAlign', 'history'],
+                inline: { options: ['bold', 'italic', 'underline', 'strikethrough'] },
+                list: { options: ['unordered', 'ordered'] },
+              }}
+              onEditorStateChange={onEditorStateChange}
+            />
+            <Typography
+              variant="caption"
+              mb={1}
+              sx={{
+                color: theme.palette.error.main,
+              }}
+            >
+              {error?.message}{' '}
+            </Typography>
+          </>
         )}
       />
     </>
